43159a596b9414adc503caeab5cd33fb7f13136a
Vo Duy Anh <voduyanh1984@gmail.com>
2023-03-28 23:38:22 +0700

    release version 0.1.2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import info from '../package.json';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Version: {info.version}
         </p>
         <a
           className="App-link"
