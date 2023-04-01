1735cd9c9904bb97d1156fd01ccaa3382abb860f
Vo Duy Anh <voduyanh1984@gmail.com>
2023-04-01 23:10:10 +0700

    next build

--- src/client/myWorkTab/MyWorkTab.tsx
+++ src/client/myWorkTab/MyWorkTab.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Provider, Flex, Text, Button, Header } from "@fluentui/react-northstar";
-import { useState, useEffect } from "react";
+import { Provider, Flex, Text, Button, Header, List } from "@fluentui/react-northstar";
+import { useState, useEffect, useCallback } from "react";
 import { useTeams } from "msteams-react-base-component";
 import { app, authentication } from "@microsoft/teams-js";
 import jwtDecode from "jwt-decode";
@@ -9,11 +9,14 @@ import jwtDecode from "jwt-decode";
  * Implementation of the my work content page
  */
 export const MyWorkTab = () => {
-
     const [{ inTeams, theme, context }] = useTeams();
     const [entityId, setEntityId] = useState<string | undefined>();
     const [name, setName] = useState<string>();
     const [error, setError] = useState<string>();
+
+    const [ssoToken, setSsoToken] = useState<string>();
+    const [msGraphOboToken, setMsGraphOboToken] = useState<string>();
+    const [recentMail, setRecentMail] = useState<any[]>();
 
     useEffect(() => {
         if (inTeams === true) {
@@ -23,6 +26,7 @@ export const MyWorkTab = () => {
             } as authentication.AuthTokenRequestParameters).then(token => {
                 const decoded: { [key: string]: any; } = jwtDecode(token) as { [key: string]: any; };
                 setName(decoded!.name);
+                setSsoToken(token);
                 app.notifySuccess();
             }).catch(message => {
                 setError(message);
@@ -42,6 +46,53 @@ export const MyWorkTab = () => {
         }
     }, [context]);
 
+    const exchangeSsoTokenForOboToken = useCallback(async () => {
+        const response = await fetch(`/exchangeSsoTokenForOboToken/?ssoToken=${ssoToken}`);
+        const responsePayload = await response.json();
+        if (response.ok) {
+            setMsGraphOboToken(responsePayload.access_token);
+        } else if (responsePayload!.error === "consent_required") {
+            setError("consent_required");
+        } else {
+            setError("unknown SSO error");
+        }
+    }, [ssoToken]);
+
+    const getRecentEmails = useCallback(async () => {
+        if (!msGraphOboToken) { return; }
+      
+        const endpoint = "https://graph.microsoft.com/v1.0/me/messages?$select=receivedDateTime,subject&$orderby=receivedDateTime&$top=10";
+        const requestObject = {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + msGraphOboToken
+          }
+        };
+      
+        const response = await fetch(endpoint, requestObject);
+        const responsePayload = await response.json();
+      
+        if (response.ok) {
+          const recentMail = responsePayload.value.map((mail: any) => ({
+            key: mail.id,
+            header: mail.subject,
+            headerMedia: mail.receivedDateTime
+          }));
+          setRecentMail(recentMail);
+        }
+    }, [msGraphOboToken]);
+
+    useEffect(() => {
+        // if the SSO token is defined...
+        if (ssoToken && ssoToken.length > 0) {
+          exchangeSsoTokenForOboToken();
+        }
+    }, [exchangeSsoTokenForOboToken, ssoToken]);
+
+    useEffect(() => {
+        getRecentEmails();
+    }, [getRecentEmails, msGraphOboToken]);
+
     /**
      * The render() method to create the UI of the tab
      */
@@ -57,6 +108,9 @@ export const MyWorkTab = () => {
                     <div>
                         <div>
                             <Text content={`Hello ${name}`} />
+                            {
+                                recentMail && <div><h3>Your recent emails:</h3><List items={recentMail} /></div>
+                            }
                         </div>
                         {error && <div><Text content={`An SSO error occurred ${error}`} /></div>}
 
